4f7b7f6cb08410b121ffffb2293df22ed306f746
unknown <nelsoneduardoac@ufps.edu.co>
2022-11-21 09:25:54 -0500

    Editando Controlador de Habilitar y Deshabilitar un Exmane

--- administracion/Controlador/Desactivar_Examen.php
+++ administracion/Controlador/Desactivar_Examen.php
@@ -8,6 +8,7 @@
       include_once '../../Conexion/Conexion.php';
       require '../../modelo/Examen.php';
       include_once '../../modelDao/ExamenDao.php';
+      include_once '../../javaScript/script_sweet.js';
 
       $codigo = $_GET['cod_examen'];
       $id_estado = $_GET['id_estado'];
@@ -19,5 +20,28 @@
       }
       $examen = new ExamenDao();
       $examen->InActivar_Examen($codigo,$estado);
-      header("location:../examenes.php");
+if($estado==2){
+      header("Refresh: 1; URL=../examenes.php");
+
+  echo '<script>
+  Swal.fire({
+   icon: "success",
+   title: "Proceso Exitoso",
+   text: "El Examen fue Deshabilitado",
+   showConfirmButton:false,
+   });
+  </script>';
+  }else{
+  header("Refresh: 1; URL=../examenes.php");
+
+  echo '<script>
+  Swal.fire({
+   icon: "success",
+   title: "Proceso Exitoso",
+   text: "El Examen fue Habilitado",
+   showConfirmButton:false,
+   });
+  </script>';
+  }
+      
       ?>
